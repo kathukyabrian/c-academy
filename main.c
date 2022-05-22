9e1003c7dda8a4f8625c5d37075cc230ff19cc89
kathukyabrian <briankathukya2000@gmail.com>
2022-05-22 23:05:12 +0300

    used malloc and calloc to allocate memory

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void decorate(){
+    printf("================================\n");
+}
+
+int main(){
+
+    printf("Using malloc to allocate memory\n");
+
+    // use malloc - returns a pointer
+    // here we are creating an array of 4 integers
+    int *intArray = malloc(4 * sizeof(int));
+
+    // could be a null pointer
+    if(intArray){
+        for(int n=0; n<4; ++n){
+            intArray[n] = n*n;
+        }
+
+        for(int n=0; n<4; ++n){
+            printf("%d element is %d\n",n, intArray[n]);
+        }
+    }
+
+    // free the memory address to avoid memory leak
+    free(intArray);
+
+
+    decorate();
+
+    printf("Using calloc to allocate memory\n");
+
+    // use  calloc - takes 2 arguments(number, size)
+    int *intArrayCalloc = calloc(4, sizeof *intArrayCalloc);
+    if(intArrayCalloc){
+        for(int n=0; n<4; ++n){
+            intArrayCalloc[n] = n*n;
+        }
+
+        for(int n=0; n<4; ++n){
+            printf("%d element is %d\n",n, intArrayCalloc[n]);
+        }
+    }
+
+    free(intArrayCalloc);
+
+
+}
