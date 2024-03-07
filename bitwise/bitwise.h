0d5495d62304ddfb0f88d95a16329809489f8e6f
kathukyabrian <briankathukya2000@gmail.com>
2024-03-07 22:03:12 +0300

    cleaned up the code using header file

--- bitwise/bitwise.h
+++ bitwise/bitwise.h
@@ -5,6 +5,17 @@
 #define WRITE_PERMISSION 0b00000010
 #define ADMIN_PERMISSION 0b00000100
 
+#define LINUX_READ_PERMISSION 0b00000100
+#define LINUX_WRITE_PERMISSION 0b00000010
+#define LINUX_EXECUTE_PERMISSION 0b00000001
+
 void checkIfEvenOrOdd(int);
+void checkLinuxPermissions(unsigned char);
+
+void decorate(){
+    printf("================================\n");
+}
+
+void swapVariables(int, int);
 
 #endif
--- bitwise/main.c
+++ bitwise/main.c
@@ -37,9 +37,76 @@ int main(){
     // scanf("%d", &evenOrOddNumber);
     // checkIfEvenOrOdd(evenOrOddNumber);
 
-    int x = 10; //1010
-    int y = 15; //1111
+    swapVariables(10, 15);
+    
+    unsigned int hex = 0;
+    unsigned char colorOne = 10;
+    unsigned char colorTwo = 20;
+    unsigned char colorThree = 30;
+    unsigned char colorFour = 10;
 
+    hex = colorOne << 24 | colorTwo << 16 | colorThree << 8 | colorFour ;
+    // 00001010 00010100 00011110 00001010
+
+    printf("the value of x is %b in binary and %x in hex\n", hex, hex);
+
+
+    // not bitwise operator gets the 2's complement of a value
+    // the result of the flipping is -(x+1)
+    unsigned char num = 100;
+    printf("not bitwise operation value %d\n", ~100);
+
+    // you can or several flags instead of using booleans
+    // if we used booleans we would use 3 bytes - each boolean flag is 3 bytes
+
+    unsigned char flags = 0;
+    flags |= READ_PERMISSION | WRITE_PERMISSION | ADMIN_PERMISSION;
+    printf("flags = %b\n", flags);
+
+    // linux file permissions uses bitwise operations
+    // chmod 777 folder
+    // 777 - file owner, group, everyone else
+    // 7 in binary is 111 meaning that user can read, write and execute
+    // 110 - 6 - read, write, no excute
+    // 100 - 4 - read only
+    checkLinuxPermissions(6);
+
+}
+
+void checkIfEvenOrOdd(int number){
+    // a number is odd if the lsb is 1
+    // if we mask it with 1 then if the output is 1 then the number is odd
+
+    // 10 = 0000 1010
+    // 1 =  0000 0001
+
+    // 0000 1010
+    // 0000 0001
+    // ---------
+    // 0000 0000
+    // ---------
+    // there 10 is an odd number
+    int result = number & 1;
+    if(result == 1){
+        printf("%d is an odd number\n");
+    }else{
+        printf("%d is an even number\n");
+    }
+}
+
+void checkLinuxPermissions(unsigned char linuxPermission){
+    decorate();
+    printf("linux permission in binary 0b%b\n", linuxPermission);
+
+    printf("This user can read %b\n", (linuxPermission & LINUX_READ_PERMISSION) ==  LINUX_READ_PERMISSION);
+    printf("This user can write %b\n", (linuxPermission & LINUX_WRITE_PERMISSION) == LINUX_WRITE_PERMISSION);
+    printf("This user can execute %b\n", (linuxPermission & LINUX_EXECUTE_PERMISSION) == LINUX_EXECUTE_PERMISSION);
+    // 100 & 111 = 100
+    decorate();
+}
+
+void swapVariables(int x, int y){
+    decorate();
     printf("value of x before swapping is %d\n", x);
     printf("value of y before swapping is %d\n", y);
 
@@ -84,66 +151,5 @@ int main(){
 
     printf("value of x after swapping is %d\n", x);
     printf("value of y after swapping is %d\n", y);
-
-
-    unsigned int hex = 0;
-    unsigned char colorOne = 10;
-    unsigned char colorTwo = 20;
-    unsigned char colorThree = 30;
-    unsigned char colorFour = 10;
-
-    hex = colorOne << 24 | colorTwo << 16 | colorThree << 8 | colorFour ;
-    // 00001010 00010100 00011110 00001010
-
-    printf("the value of x is %b in binary and %x in hex\n", hex, hex);
-
-
-    // not bitwise operator gets the 2's complement of a value
-    // the result of the flipping is -(x+1)
-    unsigned char num = 100;
-    printf("not bitwise operation value %d\n", ~100);
-
-    // you can or several flags instead of using booleans
-    // if we used booleans we would use 3 bytes - each boolean flag is 3 bytes
-
-    unsigned char flags = 0;
-    flags |= READ_PERMISSION | WRITE_PERMISSION | ADMIN_PERMISSION;
-    printf("flags = %b\n", flags);
-
-    // linux file permissions uses bitwise operations
-    // chmod 777 folder
-    // 777 - file owner, group, everyone else
-    // 7 in binary is 111 meaning that user can read, write and execute
-    // 110 - 6 - read, write, no excute
-    // 100 - 4 - read only
-
-    unsigned char linuxPermission = 4;
-    printf("linux permission in binary %b\n", linuxPermission);
-
-    printf("This user can read %b\n", (linuxPermission & 0b100) ==  0b100);
-    printf("This user can write %b\n", (linuxPermission & 0b010) == 0b010);
-    printf("This user can execute %b\n", (linuxPermission & 0b001) == 0b001);
-    // 100 & 111 = 100
-
-}
-
-void checkIfEvenOrOdd(int number){
-    // a number is odd if the lsb is 1
-    // if we mask it with 1 then if the output is 1 then the number is odd
-
-    // 10 = 0000 1010
-    // 1 =  0000 0001
-
-    // 0000 1010
-    // 0000 0001
-    // ---------
-    // 0000 0000
-    // ---------
-    // there 10 is an odd number
-    int result = number & 1;
-    if(result == 1){
-        printf("%d is an odd number\n");
-    }else{
-        printf("%d is an even number\n");
-    }
+    decorate();
 }
